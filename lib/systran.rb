7d59a89573616d849b0ecae824e671373ed46b53
alex <tgh88@mail.ru>
2019-08-19 13:52:17 +0300

    fix module systran

--- lib/systran.rb
+++ lib/systran.rb
@@ -1,9 +1,6 @@
-require 'httparty'
 require 'dotenv/load'
 require_relative 'systran/version'
 require_relative 'systran/client'
 require_relative 'systran/translate/translate'
 
-module Systran
-  class Error < StandardError; end
-end
+module Systran; end
